9b0aa5eec7c7745bd20a153d42977213af91242b
Ruslan Prokopchuk <fer.obbee@gmail.com>
2018-10-01 18:48:35 +1000

    use varargs in fmt

--- audio/fmt.zig
+++ audio/fmt.zig
@@ -7,9 +7,9 @@ fn fmtCallback(result: *[]const u8, output: []const u8) FmtErrors!void {
     result.* = output;
 }
 
-pub fn fmt(comptime s: []const u8, arg: var) []const u8 {
+pub fn fmt(comptime s: []const u8, args: ...) []const u8 {
     var result: []const u8 = undefined;
-    try std.fmt.format(&result, FmtErrors, fmtCallback, s, arg);
+    try std.fmt.format(&result, FmtErrors, fmtCallback, s, args);
     return result;
 }
 
